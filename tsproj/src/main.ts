e5e0321a55935b5ee62dac451835f3a9d4959a1a
Qianmo <wang_yuxuan@bupt.edu.cn>
2022-09-30 22:02:44 +0800

    demo

--- tsproj/src/main.ts
+++ tsproj/src/main.ts
@@ -1,23 +1,70 @@
-import './style.css'
-//import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import {
+    Engine3D,
+    Scene3D,
+    Object3D,
+    Camera3D,
+    ForwardRenderJob,
+    HDRLitMaterial,
+    BoxGeometry,
+    MeshRenderer,
+    DirectLight,
+    HoverCameraController,
+    Color,
+    ComponentBase
+} from "@orillusion/core";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="src/typescript.svg" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+class RotateScript extends ComponentBase {
+    public update() {
+        // update lifecycle codes
+        this.object3D.rotationY += 1;
+    }
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+async function demo() {
+    // initializa engine
+    await Engine3D.init({});
+    // create new scene as root node
+    let scene3D = new Scene3D();
+
+    // create camera
+    let cameraObj = new Object3D();
+    let camera = cameraObj.addComponent(Camera3D);
+    // adjust camera view
+    camera.perspective(60, window.innerWidth / window.innerHeight, 1, 5000.0);
+    // set camera controller
+    cameraObj.addComponent(HoverCameraController);
+    // add camera node
+    scene3D.addChild(cameraObj);
+
+    // create light
+    let light: Object3D = new Object3D();
+    // add direct light component
+    let component = light.addComponent(DirectLight);
+    // adjust lighting
+    component.lightColor = new Color(1.0, 1.0, 1.0, 1.0);
+    component.intensity = 1;
+    // add light object
+    scene3D.addChild(light);
+
+    // create new object
+    const obj= new Object3D();
+    // add MeshRenderer
+    let mr = obj.addComponent(MeshRenderer);
+    // set geometry
+    mr.geometry = new BoxGeometry(5, 5, 5);
+    // set material
+    mr.material = new HDRLitMaterial();
+
+    // add script
+    obj.addComponent(RotateScript)
+
+    // add object
+    scene3D.addChild(obj);
+
+    // create new forward rendering job
+    let renderJob = new ForwardRenderJob(scene3D);
+    // start rendering
+    Engine3D.startRender(renderJob);
+}
+
+demo();
